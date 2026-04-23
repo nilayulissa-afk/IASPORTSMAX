cc9b26b7d14b95ce8d42f5dc26ccd05453f832b6
nilayulissa-afk <nilayulissa@gmail.com>
2026-04-23 16:52:38 -0700

    Update claude.js

--- netlify/functions/claude.js
+++ netlify/functions/claude.js
@@ -7,8 +7,8 @@ exports.handler = async function (event) {
     let requestBody;
     if (body.content) {
       requestBody = {
-        model: "claude-sonnet-4-5",
-        max_tokens:2500,
+        model: "claude-haiku-4-5-20251001",
+        max_tokens:4000,
         messages: [{ role: "user", content: body.content }],
       };
     } else {
